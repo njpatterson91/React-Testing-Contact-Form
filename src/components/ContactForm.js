2a9257bb983b716cb2b164b222106a9f6c2a908e
Nathaniel Patterson <odst0016@gmail.com>
2020-10-01 15:45:57 -0400

    mvp done

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -14,11 +14,12 @@ const ContactForm = () => {
     <div className="App">
       <form onSubmit={handleSubmit(onSubmit)}>
         <div>
-          <label htmlFor="firstName">First Name*</label>
+          <label htmlFor="firstName">First Name</label>
           <input
+            id="firstName"
             name="firstName"
             placeholder="Edd"
-            ref={register({ required: true, maxLength: 3 })}
+            ref={register({ required: true, minLength: 3 })}
           />
           {errors.firstName && (
             <p>Looks like there was an error: {errors.firstName.type}</p>
@@ -26,8 +27,9 @@ const ContactForm = () => {
         </div>
 
         <div>
-          <label htmlFor="lastName">Last Name*</label>
+          <label htmlFor="lastName">Last Name</label>
           <input
+            id="lastName"
             name="lastName"
             placeholder="Burke"
             ref={register({ required: true })}
@@ -39,16 +41,20 @@ const ContactForm = () => {
 
         <div>
           <label htmlFor="email" placeholder="bluebill1049@hotmail.com">
-            Email*
+            Email
           </label>
-          <input name="email" ref={register({ required: true })} />
+          <input name="email" id="email" ref={register({ required: true })} />
           {errors.email && (
             <p>Looks like there was an error: {errors.email.type}</p>
           )}
         </div>
         <div>
           <label htmlFor="message">Message</label>
-          <textarea name="message" ref={register({ required: false })} />
+          <textarea
+            name="message"
+            id="message"
+            ref={register({ required: false })}
+          />
         </div>
         {data && (
           <pre style={{ textAlign: "left", color: "white" }}>
